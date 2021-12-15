6637fa5dbbd42d786760e4e0c02ad2a12605f6af
slava <namco587@gmail.com>
2021-12-15 15:25:55 +0500

    jscode

---
+++ script.js
@@ -0,0 +1,17 @@
+var Name = prompt('Введите ваше имя');
+console.log('Меня зовут ' + Name);
+var myAge = +prompt('Введите ваш возраст');
+console.log('Мне ' + myAge);
+
+var number1 = +prompt('Решите пример 6 + 3 = ?');
+console.log('Пример 1: 6 + 3 = 9 Ваш ответ ' + number1);
+var number2 = +prompt('Решите пример 20 - 5 = ?');
+console.log('Пример 2: 20 - 5 = 15 Ваш ответ ' + number2);
+var number3 = +prompt('Решите пример 15 * 2 = ?');
+console.log('Пример 3: 15 * 2 = 30 Ваш ответ ' + number3);
+var number4 = +prompt('Решите пример 8 / 4 = ?');
+console.log('Пример 4: 8 / 4 = 2 Ваш ответ ' + number4);
+var number5 = +prompt('Решите пример 10 % 3 = ?');
+console.log('Пример 5: 10 % 3 = 1 Ваш ответ ' + number5);
+
+alert('Откройте консоль для просмотра')
